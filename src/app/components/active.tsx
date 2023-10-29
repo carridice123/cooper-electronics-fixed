9c9a5c48ca8dd177fb9aaea2c705c608e0b31182
Raheem Carridice <carridiceraheem871@gmail.com>
2023-10-29 15:49:56 -0500

    use react router to help desktop nav accessibility

---
+++ src/app/components/active.tsx
@@ -0,0 +1,31 @@
+// <section className={styles.sec}>
+//   <section className={styles.sec2}>
+//     <nav className={styles.nav}>
+//       <ul className={styles.navul}>
+//         <div className={styles.gobackdiv}>
+//           <Link href="/" className={styles.goback}>
+//             <Image
+//               src="/arrowleft.png"
+//               alt="go back img"
+//               width={20}
+//               height={20}
+//             ></Image>
+//             Go back
+//           </Link>
+//         </div>
+//         <li className={styles.navli}>
+//           <Link href="/electronics">Electronics</Link>
+//         </li>
+//         <li className={styles.navli}>
+//           <Link href="/rental">Rental Cars</Link>
+//         </li>
+//         <li className={styles.navli}>
+//           <Link href="/contruction">Construction</Link>
+//         </li>
+//         <li className={styles.navli}>
+//           <Link href="/entertainment">Entertainment</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   </section>
+// </section>
--- src/app/components/secnav.tsx
+++ src/app/components/secnav.tsx
@@ -1,39 +1,49 @@
+"use client";
 import styles from "../styles/secondnav.module.css";
-import Link from "next/link";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import React from "react";
 
 const Secondnav = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const navLinks = [
+    // { title: "Home", link: "/" },
+    { title: "Electronics", link: "/electronics" },
+    { title: "Contruction", link: "/contruction" },
+    { title: "Cars on Rental", link: "/rental" },
+    { title: "Entertainment", link: "/entertainment" },
+  ];
+
   return (
-    <section className={styles.sec}>
-      <section className={styles.sec2}>
-        <nav className={styles.nav}>
-          <ul className={styles.navul}>
-            <div className={styles.gobackdiv}>
-              <Link href="/" className={styles.goback}>
-                <Image
-                  src="/arrowleft.png"
-                  alt="go back img"
-                  width={20}
-                  height={20}
-                ></Image>
-                Go back
-              </Link>
-            </div>
-            <li className={styles.navli}>
-              <Link href="/electronics">Electronics</Link>
-            </li>
-            <li className={styles.navli}>
-              <Link href="/rental">Rental Cars</Link>
-            </li>
-            <li className={styles.navli}>
-              <Link href="/contruction">Construction</Link>
-            </li>
-            <li className={styles.navli}>
-              <Link href="/entertainment">Entertainment</Link>
-            </li>
-          </ul>
-        </nav>
-      </section>
+    <section className={styles.navul}>
+      <div className={styles.gobackdiv}>
+        <Link href="/" className={styles.goback}>
+          <Image
+            src="/arrowleft.png"
+            alt="go back img"
+            width={20}
+            height={20}
+          ></Image>
+          Go back
+        </Link>
+      </div>
+      <div className={styles.divlis}>
+        {navLinks.map(({ link, title }) => (
+          <Link
+            key={title}
+            href={link}
+            className={`${
+              pathname === link
+                ? `border-solid border-b-4 border-color: rgb(0 0 0);`
+                : ``
+            } border-black border-solid`}
+          >
+            {title}
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
